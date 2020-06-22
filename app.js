39d07cfcce2dffcd2fa06f7b6aa1921098fcb6d7
ElverArevalo <elver.arevalo@gmail.com>
2020-06-22 14:22:48 -0500

    =subir a heroku1

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ productosRoutes = require('./routes/productos');
 // Conexion a la base de datos 
 mongoose.connection.openUri('mongodb://ezequiel:Samuel1to11@ds143707.mlab.com:43707/heroku_fz3c0hh7', (err, res) => {
 if (err) throw err; // si hay un error el throw no hace seguir mas alla
-console.log('Base de datos: online mongodb://localhost:27017/catalogoBD');
+
 })
 
 
@@ -79,4 +79,4 @@ app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/backend-serve/app.js'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
